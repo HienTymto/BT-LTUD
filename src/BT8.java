3ef1b0e85b4d73b89f972691a36de5934d5c1ce6
HienTymto <vuvanhien2210@gmail.com>
2023-02-28 15:28:18 +0700

    Add BT8

---
+++ src/BT8.java
@@ -0,0 +1,16 @@
+
+public class BT8 {
+
+	public static void main(String[] args) {
+		int n=1,total=0;
+		while(n<11) {
+			total+=Math.pow(n,2);
+			n++;
+		}
+		
+		System.out.println("Tong binh phuong tu 1 den 10 la: " + total);
+	}
+
+	}
+
+
