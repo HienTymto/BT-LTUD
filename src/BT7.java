2876ff386bf42c03b3bc537aff57b2dde5d4049a
HienTymto <vuvanhien2210@gmail.com>
2023-02-28 15:11:57 +0700

    add BT7

---
+++ src/BT7.java
@@ -0,0 +1,14 @@
+
+public class BT7 {
+
+	public static void main(String[] args) {
+		int n=1,total=0;
+		while(n<11) {
+			total+=n;
+			n++;
+		}
+		
+		System.out.println("Tong tu 1 den 10 la: " + total);
+	}
+
+}
